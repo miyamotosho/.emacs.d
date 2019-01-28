ab57518dec86fc7c235150131bde2cdaea4f50fe
miyamotosho <37794920+miyamotosho@users.noreply.github.com>
2019-01-28 16:10:03 +0900

    Replace places by savefiles

--- init.el
+++ init.el
@@ -35,9 +35,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(package-selected-packages
    (quote
-    (markdown-mode web-mode multiple-cursors uuidgen magit clojure aggressive-indent rainbow-delimiters try which-key exec-path-from-shell use-package smartparens evil-nerd-commenter evil))))
+    (neotree zenburn-theme markdown-mode web-mode multiple-cursors uuidgen magit clojure aggressive-indent rainbow-delimiters try which-key exec-path-from-shell use-package smartparens evil-nerd-commenter evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
